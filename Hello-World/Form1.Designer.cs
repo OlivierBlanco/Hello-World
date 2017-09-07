d0da0dacbf2453d2bd3f69cdfaea769db499feaa
Lol <olivier_blanco@hotmail.com>
2017-09-07 14:37:26 +0200

    Parametrer WinForm principale

--- Hello-World/Form1.Designer.cs
+++ Hello-World/Form1.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1455, 807);
+            this.MinimumSize = new System.Drawing.Size(600, 450);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello-World";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
